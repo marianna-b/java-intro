ff88344f6cb786da0db9a01bbf560a3f6eb3254f
Marianna Bisyarina <bisyarinamariashka@gmail.com>
2014-04-28 22:59:39 +0400

    Added alltypefunction interface

--- src/parser/Settings.java
+++ src/parser/Settings.java
@@ -15,25 +15,52 @@ public class Settings {
     public static Map<String, Function<BigInteger>> functionsBigInteger = new HashMap<>();
 
     static {
-        functionsInteger.put("abs", new Function<Integer>() {
-            @Override
-            public Integer run(Integer arg) {
-                return Math.abs(arg);
-            }
-        });
+        new Abs().put();
+    }
 
-        functionsDouble.put("abs", new Function<Double>() {
-            @Override
-            public Double run(Double arg) {
-                return Math.abs(arg);
-            }
-        });
+    static class Abs implements AllTypeFunctions {
 
-        functionsBigInteger.put("abs", new Function<BigInteger>() {
-            @Override
-            public BigInteger run(BigInteger arg) {
-                return arg.abs();
-            }
-        });
+        public void put() {
+            Abs abs = new Abs();
+            abs.putInteger();
+            abs.putDouble();
+            abs.putBigInteger();
+        }
+
+        public void putInteger() {
+
+            functionsInteger.put("abs", new Function<Integer>() {
+                @Override
+                public Integer run(Integer arg) {
+                    return Math.abs(arg);
+                }
+            });
+        }
+
+        public void putDouble() {
+            functionsDouble.put("abs", new Function<Double>() {
+                @Override
+                public Double run(Double arg) {
+                    return Math.abs(arg);
+                }
+            });
+        }
+
+        public void putBigInteger() {
+            functionsBigInteger.put("abs", new Function<BigInteger>() {
+                @Override
+                public BigInteger run(BigInteger arg) {
+                    return arg.abs();
+                }
+            });
+        }
+    }
+
+    static interface AllTypeFunctions {
+        void put();
+
+        void putInteger();
+        void putDouble();
+        void putBigInteger();
     }
 }
